9180b41f1acd6d2045a44af0ddbf57edbd12f478
Soham <sohamd@wharton.upenn.edu>
2018-04-14 22:19:01 -0400

    fourth commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if !@user.nil? && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def delete
+    reset_session
+    redirect_to '/'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   has_many :users_orders, dependent: :destroy
   has_many :orders, through: :users_orders
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash) unless password_hash.nil?
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
